aefde602fbf0986b909a0e9da054581bf5753932
krNeko9t <liuyifei@pjlab.org.cn>
2026-01-06 21:50:46 +0800

    minor

--- convert_scannetpp_example.sh
+++ convert_scannetpp_example.sh
@@ -5,7 +5,7 @@
 SCANNETPP_PATH="/home/bingxing2/ailab/liuyifei/lyj/Dataset/scannetpp"          # ScanNet++数据集根路径
 SCENE_NAME="0a7cc12c0e"                  # 场景名称
 OUTPUT_PATH="./scannetpp"               # 输出路径
-GS_SCENE_PATH="/home/bingxing2/ailab/liuyifei/lyj/Dataset/scannetpp/0a7cc12c0e/3dgs"         # 3DGS场景路径
+GS_SCENE_PATH="/home/bingxing2/ailab/liuyifei/lyj/Dataset/scannetpp/0a7cc12c0e/3dgs/0a7cc12c0e"         # 3DGS场景路径
 
 # 运行转换脚本
 python convert_scannetpp.py \
@@ -13,6 +13,8 @@ python convert_scannetpp.py \
     --scene_name "$SCENE_NAME" \
     --output_path "$OUTPUT_PATH" \
     --gs_scene_path "$GS_SCENE_PATH"
+    --skip_step1
+    --skip_step2
 
 # 检查是否成功
 if [ $? -eq 0 ]; then
